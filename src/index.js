5554829a6ddb8c74d0c9fb28ed64df57bab4a4ef
Yuliya Lineitsava <serata@mail.ru>
2020-09-28 22:52:26 +0300

    feat: add final task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined) {
+    return 0;
+  }
+  if (!array.length) {
+     return 0;
+   }
+
+
+
+  var minScore = array[0];
+    for (var i = 1; i < array.length; i++){
+    if (array[i] < minScore){
+          minScore = array[i];
+    }} return minScore;
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined) {
+    return 0;
+  }
+  if (!array.length) {
+     return 0;
+   }
+
+var maxScore = 0;
+for (var i = 0; i < array.length; i++){
+if (array[i] > maxScore){
+      maxScore = array[i];
+}} return maxScore;
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined) {
+    return 0;
+  }
+  if (!array.length) {
+     return 0;
+   }
+
+  var sumNum = array.reduce(function(sum, current){
+      return sum + current;
+  }, 0);
+      var avgNum = sumNum/(array.length);
+      return avgNum;
+
 }
